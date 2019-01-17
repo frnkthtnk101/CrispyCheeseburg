32c2b3b24b9d8ec5a96738f83bed3a41656306d0
oscreader <oscreader@OSC>
2019-01-17 16:26:09 -0700

    homework1 assignement before questions

--- HW1/simple1.c
+++ HW1/simple1.c
@@ -1,51 +1,32 @@
+/*
+ *simle1.c
+ *the hello world for modules
+ *Franco Pettigrosso
+ */
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/types.h>
-#include <linux/list.h>
-#include <linux/slab.h>
 
-struct Birthday {
-	int day;
-	int month;
-	int year;
-	struct list_head list;
-};
-
-struct Birthday BirthdayList;
-
+/*
+ *SimpleInit()
+ *when the kernel module is initialized it will print to the kernel log
+ */
 int SimpleInit(void){
-	struct Birthday *NewBirthday, *aBirthday;
-	unsigned int i;
-	printk(KERN_INFO "Loading Module\n");
-	INIT_LIST_HEAD(&BirthdayList.list);
-	for ( i = 0 ; i < 5; i++){
-		NewBirthday = kmalloc(sizeof(*NewBirthday), GFP_KERNEL);
-		NewBirthday->day=i;
-		NewBirthday->month=3;
-		NewBirthday->year=1993;
-		INIT_LIST_HEAD(&NewBirthday->list);
-		list_add_tail(&(NewBirthday->list), &(BirthdayList.list));
-	}
-	list_for_each_entry(aBirthday, &BirthdayList.list, list){
-		printk(KERN_INFO "ADD Birthday: {day=%i,month=%i,year=%i}\n",aBirthday->day,aBirthday->month,aBirthday->year);
-	}	
+	printk(KERN_INFO "HELLO WORLD!\n");
 	return 0;
 }
-
+/*
+ *SimpleExit()
+ *when the kernel module is deconstructed it will print to the kernel log
+ */
 void SimpleExit(void){
-	struct Birthday *aBirthday, *temp;
-	printk(KERN_INFO "Removing Module and clean list up\n");
-	list_for_each_entry_safe(aBirthday,temp,&BirthdayList.list,list){
-		printk(KERN_INFO "REMOVE Birthday: {day=%i,month=%i,year=%i}\n",aBirthday->day,aBirthday->month,aBirthday->year);
-		list_del(&aBirthday->list);
-		kfree(aBirthday);
-	}
+	printk(KERN_INFO "Goodbye World!\n");
 }
 
+//methods for registering module entry point and exit points
 module_init(SimpleInit);
 module_exit(SimpleExit);
-
+//stuff about the description
 MODULE_LICENSE("FP");
 MODULE_DESCRIPTION("hello world for modules");
 MODULE_AUTHOR("Franco Pettigrosso");
