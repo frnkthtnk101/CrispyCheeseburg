3f87fcbffff454294b9a28514d0db4e8dd00e712
Franco Pettigrosso <fpettigrosso@holyfamily.edu>
2019-03-03 12:45:46 -0500

    made a bunch of the same file with different paramters to show statistics

--- HW3/resultsTesting.test.ps1
+++ HW3/resultsTesting.test.ps1
@@ -2,22 +2,21 @@
 cd C:\Users\fpettigrosso\ws\CrispyCheeseburg\HW3
 $correct = Get-Content .\correct.txt
 $results = Get-Content .\results.txt
+$ValuesCorrect = @()
+$ValuesResults = @()
+$correct | ForEach-Object {
+    $ValuesCorrect += $_.Split(" ")[-1]
+}
+$results | ForEach-Object {
+    $ValuesResults += $_.Split(" ")[-1]
+}
 cls
-Describe "testing results"{
-
-    Context "Testing lines"{
-            $i = 0
-            $t =0
-        foreach( $line in $results){
-            it "$line" {
-
-
-                $correct.Contains($line) | Should be $true
-
-
-            }
- 
+Describe "Test"{
+    Context "testing line value"{
+    for($i=0; $i -lt $correct.Count; $i++){
+    it "$($correct[$i]) vs $($results[$i])"{
+         $ValuesResults[$i] |Should be $ValuesCorrect[$i]
         }
-               
+    }
     }
 }
