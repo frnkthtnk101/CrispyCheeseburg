c45ba3fcbddf855679f2ae0b3adee69b324826c1
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-03-03 13:50:04 -0500

    have a make file and statistics

--- HW3/statistics.sh
+++ HW3/statistics.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+$echo $1
+echo making programs
+make ram1 ram3_128 ram3_128_32 ram3_128_4 ram3_256
+echo creating statistics
+./ram1 $1 $2 > ram1_Stats.txt
+./ram3_128 $1 $2 > ram3_128_Stats.txt
+./ram3_128_4 $1 $2 > ram3_128_4_Stats.txt
+./ram3_128_32 $1 $2 > ram3_128_32_Stats.txt
+./ram3_256 $1 $2 > ram3_256_Stats.txt
+echo clean
+make clean
